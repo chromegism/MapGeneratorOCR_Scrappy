475a9cc2f7386a5ca6ed5762ddea0801ad8684b5
Ruan Kellard <ruankellard@gmail.com>
2026-03-01 14:50:22 +0000

    Update /src/shaders/compile.bat for optimisation

--- src/shaders/compile.bat
+++ src/shaders/compile.bat
@@ -1,3 +1,3 @@
-C:\VulkanSDK\1.4.341.1\Bin\glslc.exe test.vert -o test.vert.spv
-C:\VulkanSDK\1.4.341.1\Bin\glslc.exe test.frag -o test.frag.spv
+C:\VulkanSDK\1.4.341.1\Bin\glslc.exe test.vert -O -o test.vert.spv
+C:\VulkanSDK\1.4.341.1\Bin\glslc.exe test.frag -O -o test.frag.spv
 pause
