2605dc3619c1c1422d216b7ffe772b038a849462
Ruan Kellard <ruankellard@gmail.com>
2026-03-02 02:39:31 +0000

    Remove Window.cpp

--- src/Window.h
+++ src/Window.h
@@ -3,18 +3,30 @@
 #include <string>
 #include <cstdint>
 
+#include <SDL3/SDL_video.h>
+#include <SDL3/SDL_init.h>
+
 struct SDL_Window;
 
 class Window {
 private:
 	SDL_Window* handle_ = nullptr;
 
-	void init();
+	void init() {
+		if (SDL_InitSubSystem(SDL_INIT_VIDEO) == false) {
+			throw std::runtime_error("Error initialising SDL3: " + (std::string)SDL_GetError());
+		} // Automatically initialises SDL_INIT_EVENTS
+
+		handle_ = SDL_CreateWindow(details_.name.c_str(), details_.width, details_.height, SDL_WINDOW_VULKAN);
+		if (!handle_) {
+			throw std::runtime_error("Error creating window: " + (std::string)SDL_GetError());
+		}
+	}
 
 public:
 	struct Details {
 		std::string name;
-		uint32_t width, height;
+		uint32_t width = 0, height = 0;
 	};
 private:
 	Details details_;
@@ -26,7 +38,7 @@ public:
 	~Window() { destroy(); }
 
 	Window(const Window&) noexcept = delete; // move only
-	Window(Window&& other) noexcept : handle_(other.handle_) {
+	Window(Window&& other) noexcept : handle_(other.handle_), details_(other.details_) {
 		other.handle_ = nullptr;
 	}
 	Window& operator=(Window&& other) noexcept {
@@ -35,6 +47,7 @@ public:
 				destroy();
 
 			handle_ = other.handle();
+			details_ = other.details();
 			other.handle_ = nullptr;
 		}
 		return *this;
@@ -44,7 +57,7 @@ public:
 	bool isValid() const noexcept { return handle_ != nullptr; }
 	const Details& details() const noexcept { return details_; }
 
-	void destroy() {
+	void destroy() noexcept {
 		if (isValid()) {
 			SDL_QuitSubSystem(SDL_INIT_VIDEO);
 			SDL_DestroyWindow(handle_);
